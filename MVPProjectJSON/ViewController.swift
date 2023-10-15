3bd40f933d4aaa52d018a81df4cc4a048f3be7d0
Danila Bolshakov <danila1999777@gmail.com>
2023-10-15 18:03:11 +0300

    Initial commit

---
+++ MVPProjectJSON/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  MVPProjectJSON
+//
+//  Created by iOS - Developer on 15.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
