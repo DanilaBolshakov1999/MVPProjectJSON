159ec4a915401f32d20671ee4ac129f01b2e55c2
Danila Bolshakov <danila1999777@gmail.com>
2023-10-15 18:36:32 +0300

    ImagesModel

---
+++ MVPProjectJSON/App/ModuleBuilder.swift
@@ -0,0 +1,9 @@
+//
+//  ModuleBuilder.swift
+//  MVPProjectJSON
+//
+//  Created by iOS - Developer on 15.10.2023.
+//
+
+import Foundation
+
---
+++ MVPProjectJSON/Modules/MainModule/Model/ImagesModel.swift
@@ -0,0 +1,32 @@
+//
+//  ModelImages.swift
+//  MVPProjectJSON
+//
+//  Created by iOS - Developer on 15.10.2023.
+//
+
+import Foundation
+
+//MARK: - ImagesModel
+struct ImagesModel: Decodable {
+    let breeds: Breeds
+    let id: String
+    let url: String
+    let width: Int
+    let height: Int
+}
+
+struct Breeds: Decodable {
+    let weight: Weight
+    let id: String
+    let name: String
+    let temperament: String
+    let origin: String
+    let country_codes: String
+    let country_code: String
+}
+
+struct Weight: Decodable {
+    let imperial: String
+    let metric: String
+}
